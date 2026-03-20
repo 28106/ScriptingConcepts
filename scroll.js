6c49a8bd0057aa749555180edbf9a5a5d050131d
Delta Student <mswapnali935@gmail.com>
2026-03-20 16:12:31 +0530

    Added Files

---
+++ scroll.js
@@ -0,0 +1,13 @@
+<html>
+<body onscroll="showMsg()" style="height:2000px">
+
+<p id="msg">Scroll the page</p>
+
+<script>
+function showMsg() {
+    document.getElementById("msg").innerHTML = "Page is being scrolled";
+}
+</script>
+
+</body>
+</html>
